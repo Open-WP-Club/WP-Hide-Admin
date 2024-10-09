e320d6ab4f9154d56a4d26305cc4a4af5ee02dc0
MrGKanev <contact@mrgkanev.eu>
2024-10-09 16:30:27 +0300

    wip

---
+++ wp-hide-admin.php
@@ -0,0 +1,199 @@
+<?php
+/*
+Plugin Name: WP Hide Admin
+Description: Hides the WordPress admin area and provides custom login URL.
+Version: 1.0
+Author: Your Name
+*/
+
+// Exit if accessed directly
+if (!defined('ABSPATH')) {
+  exit;
+}
+
+class WP_Hide_Admin
+{
+  private $options;
+
+  public function __construct()
+  {
+    add_action('admin_menu', array($this, 'add_plugin_page'));
+    add_action('admin_init', array($this, 'page_init'));
+    add_action('plugins_loaded', array($this, 'hide_admin'));
+    add_action('wp_loaded', array($this, 'custom_login_url'));
+    add_action('admin_post_export_ip_log', array($this, 'export_ip_log'));
+  }
+
+  public function add_plugin_page()
+  {
+    add_options_page(
+      'WP Hide Admin Settings',
+      'WP Hide Admin',
+      'manage_options',
+      'wp-hide-admin',
+      array($this, 'create_admin_page')
+    );
+  }
+
+  public function create_admin_page()
+  {
+    $this->options = get_option('wp_hide_admin_options');
+?>
+    <div class="wrap">
+      <h1>WP Hide Admin Settings</h1>
+      <form method="post" action="options.php">
+        <?php
+        settings_fields('wp_hide_admin_option_group');
+        do_settings_sections('wp-hide-admin-setting-admin');
+        submit_button();
+        ?>
+      </form>
+      <h2>IP Log</h2>
+      <?php $this->display_ip_log(); ?>
+      <form method="post" action="<?php echo admin_url('admin-post.php'); ?>">
+        <input type="hidden" name="action" value="export_ip_log">
+        <?php submit_button('Export IP Log', 'secondary'); ?>
+      </form>
+    </div>
+<?php
+  }
+
+  public function page_init()
+  {
+    register_setting(
+      'wp_hide_admin_option_group',
+      'wp_hide_admin_options',
+      array($this, 'sanitize')
+    );
+
+    add_settings_section(
+      'wp_hide_admin_setting_section',
+      'Settings',
+      array($this, 'print_section_info'),
+      'wp-hide-admin-setting-admin'
+    );
+
+    add_settings_field(
+      'login_url',
+      'Custom Login URL',
+      array($this, 'login_url_callback'),
+      'wp-hide-admin-setting-admin',
+      'wp_hide_admin_setting_section'
+    );
+
+    add_settings_field(
+      'redirect_url',
+      'Redirect URL for old wp-admin',
+      array($this, 'redirect_url_callback'),
+      'wp-hide-admin-setting-admin',
+      'wp_hide_admin_setting_section'
+    );
+  }
+
+  public function sanitize($input)
+  {
+    $new_input = array();
+    if (isset($input['login_url']))
+      $new_input['login_url'] = sanitize_text_field($input['login_url']);
+    if (isset($input['redirect_url']))
+      $new_input['redirect_url'] = sanitize_text_field($input['redirect_url']);
+    return $new_input;
+  }
+
+  public function print_section_info()
+  {
+    print 'Enter your settings below:';
+  }
+
+  public function login_url_callback()
+  {
+    printf(
+      '<input type="text" id="login_url" name="wp_hide_admin_options[login_url]" value="%s" />',
+      isset($this->options['login_url']) ? esc_attr($this->options['login_url']) : ''
+    );
+  }
+
+  public function redirect_url_callback()
+  {
+    printf(
+      '<input type="text" id="redirect_url" name="wp_hide_admin_options[redirect_url]" value="%s" />',
+      isset($this->options['redirect_url']) ? esc_attr($this->options['redirect_url']) : ''
+    );
+  }
+
+  public function hide_admin()
+  {
+    $this->options = get_option('wp_hide_admin_options');
+    if (isset($this->options['login_url']) && !empty($this->options['login_url'])) {
+      add_action('init', array($this, 'block_wp_admin'));
+    }
+  }
+
+  public function block_wp_admin()
+  {
+    if (!is_user_logged_in() && $GLOBALS['pagenow'] === 'wp-login.php') {
+      $redirect_url = home_url('/');
+      if (isset($this->options['redirect_url']) && !empty($this->options['redirect_url'])) {
+        $redirect_url = $this->options['redirect_url'];
+      }
+      $this->log_attempt();
+      wp_redirect($redirect_url);
+      exit;
+    }
+  }
+
+  public function custom_login_url()
+  {
+    $this->options = get_option('wp_hide_admin_options');
+    if (isset($this->options['login_url']) && !empty($this->options['login_url'])) {
+      $custom_login_url = home_url($this->options['login_url']);
+      if ($_SERVER['REQUEST_URI'] == '/' . $this->options['login_url']) {
+        require_once(ABSPATH . 'wp-login.php');
+        exit;
+      }
+    }
+  }
+
+  private function log_attempt()
+  {
+    $ip_log = get_option('wp_hide_admin_ip_log', array());
+    $ip = $_SERVER['REMOTE_ADDR'];
+    if (isset($ip_log[$ip])) {
+      $ip_log[$ip]++;
+    } else {
+      $ip_log[$ip] = 1;
+    }
+    update_option('wp_hide_admin_ip_log', $ip_log);
+  }
+
+  public function display_ip_log()
+  {
+    $ip_log = get_option('wp_hide_admin_ip_log', array());
+    if (!empty($ip_log)) {
+      echo '<table class="wp-list-table widefat fixed striped">';
+      echo '<thead><tr><th>IP Address</th><th>Attempts</th></tr></thead>';
+      echo '<tbody>';
+      foreach ($ip_log as $ip => $attempts) {
+        echo "<tr><td>$ip</td><td>$attempts</td></tr>";
+      }
+      echo '</tbody></table>';
+    } else {
+      echo '<p>No attempts logged yet.</p>';
+    }
+  }
+
+  public function export_ip_log()
+  {
+    $ip_log = get_option('wp_hide_admin_ip_log', array());
+    $csv = "IP Address,Attempts\n";
+    foreach ($ip_log as $ip => $attempts) {
+      $csv .= "$ip,$attempts\n";
+    }
+    header('Content-Type: text/csv');
+    header('Content-Disposition: attachment; filename="wp_hide_admin_ip_log.csv"');
+    echo $csv;
+    exit;
+  }
+}
+
+$wp_hide_admin = new WP_Hide_Admin();
